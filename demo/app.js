031ba2f0770d3ccf1cf819cac8d30cc2a8f3eb1c
Bas <30810@ma-web.nl>
2021-01-12 11:20:51 +0100

    de demo

---
+++ demo/app.js
@@ -0,0 +1,40 @@
+// voeg controls toe (teleport & WASD controls)
+const controls = new Controls(50);
+controls.enableMovement();
+
+// create a model
+const minecraftModel = new Model('assets/models/minecraft/scene.gltf');
+minecraftModel.setPosition(0, 1.54, -5);
+minecraftModel.setScale(0.1, 0.1, 0.1);
+minecraftModel.setRotation(0, -36.95, 0);
+
+// create a primitive (sphere)
+const sphere = new Sphere();
+sphere.setColor('green');
+sphere.setRadius(0.5);
+sphere.setPosition(-3, 0, -5);
+
+// create some text
+const testText = new Text('Mediacollege Amsterdam WebXR');
+testText.setPosition(1.5, 0, -5);
+testText.setFontsize(10);
+testText.setColor('wheat')
+
+// create an image
+const image = new XRImage('assets/images/sampleImage.jpg');
+image.setPosition(-1.48819, 2.45839, -5);
+
+// create a 360 image
+const image360 = new Sky('assets/images/sampleImage.jpg');
+
+const spotLight = new SpotLight();
+spotLight.setPosition(-3.39382, 4.72931, 1.37905);
+spotLight.setTarget(minecraftModel);
+
+// listen for an event
+minecraftModel.addEventListener('click', () => console.log('je klikt op mij'));
+
+const cuberoom = new Model('assets/models/cube-room/cube-room.gltf');
+cuberoom.setPosition(-3.5, 1, 1);
+cuberoom.setScale(0.1, 1, 0.1);
+cuberoom.setRotation(0, -37, 0);
